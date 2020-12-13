2449e16460c314b77b892ad8e569fee223b7809b
unilucid <75928826+unilucid@users.noreply.github.com>
2020-12-13 17:49:01 +0900

    Add files via upload

---
+++ repetition.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("C언어 재미있네요! \n");
+	printf("C언어 재미있네요! \n");
+	printf("C언어 재미있네요! \n");
+
+	int n = 1;
+
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+
+	return 0;
+}
